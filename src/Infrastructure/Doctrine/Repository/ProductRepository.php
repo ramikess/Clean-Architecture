76b4271cb1cc8f750bbb6ae5843f32ada7a4f7b2
kessentini <rami.kessentini@poudebon.com>
2024-09-14 15:22:52 +0200

    mapping orm with xml

--- src/Infrastructure/Doctrine/Repository/ProductRepository.php
+++ src/Infrastructure/Doctrine/Repository/ProductRepository.php
@@ -4,22 +4,19 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Doctrine\Repository;
 
-use App\Domain\Entity\Product as ProductDomain;
+use App\Domain\Entity\Product;
 use App\Domain\Repository\ProductRepositoryInterface;
-use App\Infrastructure\DataMapper\ProductEntityMapper;
-use App\Infrastructure\Doctrine\Entity\Product as ProductDoctrine;
 use Doctrine\ORM\EntityManagerInterface;
 
 class ProductRepository implements ProductRepositoryInterface
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-        private readonly ProductEntityMapper $productEntityMapper
     ) { }
 
-    public function saveProduct(ProductDomain $entry): void
+    public function saveProduct(Product $entry): void
     {
-        $product = new ProductDoctrine();
+        $product = new Product();
         $product->setName($entry->getName());
         $product->setPrice($entry->getPrice());
         $product->setDescription($entry->getDescription());
@@ -28,9 +25,8 @@ class ProductRepository implements ProductRepositoryInterface
         $this->entityManager->flush();
     }
 
-    public function searchProduct(int $id): ProductDomain
+    public function searchProduct(int $id): Product
     {
-        $productDoctrine = $this->entityManager->getRepository(ProductDoctrine::class)->find($id);
-        return $this->productEntityMapper->convertToDomain($productDoctrine);
+        return $this->entityManager->getRepository(Product::class)->find($id);
     }
 }
