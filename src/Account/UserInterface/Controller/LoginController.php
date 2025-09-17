b31d3adb1ee7017f6bff7cf1405e7ab5f5a44885
kessentini <rami.kessentini@poudebon.com>
2025-09-17 13:28:44 +0200

    login error message

--- src/Account/UserInterface/Controller/LoginController.php
+++ src/Account/UserInterface/Controller/LoginController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Account\UserInterface\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -12,15 +14,18 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LoginController extends AbstractController
 {
 
-    public function __invoke(AuthenticationUtils $authenticationUtils)
+    public function __invoke(AuthenticationUtils $authenticationUtils): RedirectResponse|Response
     {
         if ($this->getUser() !== null) {
             return $this->redirectToRoute('app_homepage');
         }
 
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('@Account/User/login.html.twig', [
-            'last_username' => $authenticationUtils->getLastUsername(),
-            'error' => $authenticationUtils->getLastAuthenticationError(),
+            'last_username' => $lastUsername,
+            'error_message' => $error?->getMessage(),
         ]);
     }
 }
