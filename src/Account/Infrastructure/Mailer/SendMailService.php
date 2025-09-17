62398b64fe3bb669ea4b505d89a12d621d14b105
kessentini <rami.kessentini@poudebon.com>
2025-09-17 10:46:54 +0200

    clearing

--- src/Account/Infrastructure/Mailer/SendMailService.php
+++ src/Account/Infrastructure/Mailer/SendMailService.php
@@ -18,13 +18,9 @@ class SendMailService implements NotificationInterface
         $email = (new Email())
             ->from('hello@example.com')
             ->to('you@example.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
             ->subject('Time for Symfony Mailer!')
             ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
+            ->html('<p>Hello !</p>');
 
         $this->mailer->send($email);
     }
