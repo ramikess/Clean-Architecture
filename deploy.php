fa915e4c6e8504a49c0e5ef92f948b1c153dd8d1
Rami <rami.kessentini@gmail.com>
2025-12-08 17:50:36 +0100

    clear

--- deploy.php
+++ deploy.php
@@ -18,18 +18,6 @@ host('178.128.41.81')
     ->set('deploy_path', '~/applications/zqwhxgdehy/public_html')
 ;
 
-// 1. Upload .env.prod as .env.local
-task('deploy:upload_env', function () {
-    upload('.env.prod', '{{deploy_path}}/shared/.env.local');
-    run('chmod 600 {{deploy_path}}/shared/.env.local');
-});
-
-
-// Custom task for Composer install
-task('deploy:vendors', function () {
-    run('cd {{release_path}} && composer install --prefer-dist --no-dev --optimize-autoloader --ignore-platform-req=ext-amqp');
-});
-
 // Hooks
 
 after('deploy:failed', 'deploy:unlock');
