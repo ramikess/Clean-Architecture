f7cb18f50d9b3ef1e03ab5c585be0f1489d0f3b1
Rami <rami.kessentini@gmail.com>
2025-12-09 10:34:02 +0100

    clear

--- deploy.php
+++ deploy.php
@@ -24,9 +24,9 @@ host('178.128.41.81')
 // ============================================================================
 
 // Installation des dépendances
-//task('deploy:vendors', function () {
-//    run('cd {{release_path}} && composer install --prefer-dist --no-dev --optimize-autoloader --ignore-platform-req=ext-amqp');
-//});
+task('deploy:vendors', function () {
+    run('cd {{release_path}} && composer install --prefer-dist --no-dev --optimize-autoloader --ignore-platform-req=ext-amqp');
+});
 
 task('deploy:copy_env', function () {
     // Crée shared/.env s'il n'existe pas
@@ -58,8 +58,8 @@ task('deploy:cache_warmup', function () {
 // ============================================================================
 task('deploy', [
     'deploy:prepare',
-//    'deploy:vendors',
     'deploy:copy_env',
+    'deploy:vendors',
     'deploy:cache_safe',
     'deploy:cache_warmup',
     'deploy:publish',
