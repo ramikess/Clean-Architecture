22e5230991b60f36116908b76855adb8ebbd44f3
Rami <rami.kessentini@gmail.com>
2025-12-10 10:10:24 +0100

    clean

--- deploy.php
+++ deploy.php
@@ -8,7 +8,7 @@ require 'recipe/symfony.php';
 // ============================================================================
 set('repository', 'git@github.com:ramikess/Clean-Architecture.git');
 
-add('shared_files', ['.env', '.env.local']);
+add('shared_files', []);
 add('shared_dirs', []);
 set('writable_dirs', ['var']);
 
@@ -24,6 +24,20 @@ host('production')
 // ============================================================================
 // TASKS PERSONNALISÉES
 // ============================================================================
+
+task('deploy:copy_env', function () {
+    // Crée shared/.env s'il n'existe pas
+    run('if [ ! -f {{deploy_path}}/shared/.env ]; then touch {{deploy_path}}/shared/.env; fi');
+
+    // Copie .env.prod vers shared/.env
+    run('cp {{release_path}}/.env.prod {{deploy_path}}/shared/.env;');
+
+    // Copie .env.prod vers shared/.env.local)
+    run('cp {{release_path}}/.env.prod {{deploy_path}}/shared/.env.local');
+
+    // Crée le lien symbolique de shared/.env vers la release
+    run('ln -s {{deploy_path}}/shared/.env {{release_path}}/.env');
+});
 
 // Installation des dépendances
 task('deploy:vendors', function () {
@@ -46,6 +60,7 @@ task('deploy:cache_warmup', function () {
 // ============================================================================
 task('deploy', [
     'deploy:prepare',
+    'deploy:copy_env',
     'deploy:vendors',
     'deploy:cache_safe',
     'deploy:cache_warmup',
